a99782f84b2787cf9f0968a771dc5af6cbf378ab
Fernanda <fernandamancini07@gmail.com>
2023-02-07 13:37:43 -0300

    Atualização

---
+++ site-main-main/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{html,js,jsx}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "desafios": "url('../public/img/desafios.png')",
+        "inspiradoras": "url('../public/img/inspiradoras.png')",
+        "cursosVagas": "url('../public/img/cursos/cursos_vagas.jpg')",
+        "sobre": "url('../public/img/sobre.png')"
+      }
+    },
+  },
+  plugins: [],
+};
